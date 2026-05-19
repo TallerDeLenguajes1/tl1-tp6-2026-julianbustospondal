f3049e2a346d0f9a8b584ff29b0e525486e61f3f
julianbustos <julianbustospondal@gmail.com>
2026-05-19 15:19:10 -0300

    ejercicio 1

--- Program.cs
+++ Program.cs
@@ -1,7 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
-int a;
-int b;
-a = 10;
-b = a;
-Console.WriteLine("valor de a:" + a);
-Console.WriteLine("valor de b:" + b);
+﻿// Console.WriteLine("Hello, World!");
+// int a;
+// int b;
+// a = 10;
+// b = a;
+// Console.WriteLine("valor de a:" + a);
+// Console.WriteLine("valor de b:" + b);
+
+//ejercicio 1
+int numero;
+Console.WriteLine("ingrese un número entero:");
+if (int.TryParse(Console.ReadLine(), out numero))
+{
+    int aux;
+
+    int numeroInvertido = 0;
+    while (numero > 0)
+    {
+        aux = numero % 10;
+        numeroInvertido = (numeroInvertido * 10) + aux;
+        numero = numero / 10;
+    }
+    Console.WriteLine("El número invertido es: " + numeroInvertido);
+
+}
+else
+{
+    Console.WriteLine("Entrada no válida. Por favor, ingrese un número entero.");
+}
